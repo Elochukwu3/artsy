cda083fd80192889c79df5b56598fbb96f4669fd
Elochukwu3 <oguajuromauld5@gmail.com>
2022-12-02 11:35:22 +0100

    icons

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,10 @@
-import React from 'react'
+
+import NaBar from './NaBar'
 
 export default function Layout({children}) {
   return (
     <div>
-        <h1>Layout</h1>
+        <NaBar/>
         <section>
             {children}
         </section>
--- components/NaBar.js
+++ components/NaBar.js
@@ -1,14 +1,25 @@
 import React from 'react'
-
+const MENU_HOVER = "text-2xl cursor-pointer font-normal after:content-[''] after:duration-300 after:h-1 after:block after:w-0 after:bg-slate-400 hover:after:w-full"
 export default function NaBar() {
   return (
-    <div>
-        <nav>
+    <div className='h-auto px-20'>
+        <nav className='grid grid-cols-3 py-10 items-center bg-white'>
             <section className='logo'>
-                <img src='./nav-img/ARTSY.png'/>
+                <img src='nav-img/ARTSY.png'/>
             </section>
-            <section className='menu'></section>
-            <section className='icons'></section>
+            <section className='menu'>
+                <ul className='flex gap-10'>
+                    <li className={MENU_HOVER}>Home</li>
+                    <li className={MENU_HOVER} >MarketPlace</li>
+                    <li className={MENU_HOVER} >Auction</li>
+                    <li className={MENU_HOVER} >Drop</li>
+                </ul>
+            </section>
+            <section className='icons'>
+                <img src='nav_img/search.png' alt='icon'/>
+                <img src='./nav_img/cart.png'/>
+                <img src='./nav_img/notify.png'/>
+            </section>
         </nav>
     </div>
   )
